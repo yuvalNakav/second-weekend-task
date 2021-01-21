60edf6825bc9b9de150b0e17fb1b9f5753938866
yuvalNakav <yuvalnakav1@gmail.com>
2021-01-21 16:11:32 +0200

    removed functions and added a for... of loop for totalTime and taskFinishedPercent

--- index.js
+++ index.js
@@ -1,100 +1,88 @@
 const tasks = [
     {
         startedAt: date1 =new Date (2021, 1, 10, 16)  ,
-        finishedAt: date2 = new Date (2021, 1, 10, 20) ,
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '10',
-        tasksFinished: '8',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
+        finishedAt: date2 = new Date(2021, 1, 10, 20),
+        tasksGiven: 10,
+        tasksFinished: 8,
         topic: 'JavaScript'
     },
     {
         startedAt: date1 = new Date (2021, 1, 11, 13) ,
         finishedAt: date2 = new Date (2021, 1, 11, 19) ,
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '8',
-        tasksFinished: '4',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        tasksGiven: 8,
+        tasksFinished: 4,
+        topic: 'HTML'
     },
     {
         startedAt: date1 = new Date (2021, 1, 12, 14),
         finishedAt: date2 = new Date (2021, 1, 12, 19),
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '10',
-        tasksFinished: '7',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        tasksGiven: 10,
+        tasksFinished: 7,
+        topic: 'HTML'
     },
     {
         startedAt: date1 = new Date (2021, 1, 13, 13) ,
         finishedAt: date2 = new Date (2021, 1, 13, 15) ,
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '20',
-        tasksFinished: '18',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        tasksGiven: 20,
+        tasksFinished: 18,
+        topic: 'CSS'
     },
     {
         startedAt: date1 = new Date (2021, 1, 14, 14) ,
         finishedAt: date2 = new Date (2021, 1, 14, 18), 
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '10',
-        tasksFinished: '10',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
+        tasksGiven: 10,
+        tasksFinished: 10,
         topic: 'JavaScript'
     },
     {
         startedAt: date1 = new Date (2021, 1, 15, 15) ,
         finishedAt: date2 = new Date (2021, 1, 15, 19) ,
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '8',
-        tasksFinished: '8',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        tasksGiven: 8,
+        tasksFinished: 8,
+        topic: 'CSS'
     },
     {
         startedAt: date1 = new Date (2021, 1, 16, 16) ,
-        finishedAt: date2  = new Date (2021, 1, 16, 18)
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '15',
-        tasksFinished: '12',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        finishedAt: date2  = new Date (2021, 1, 16, 18),
+        tasksGiven: 15,
+        tasksFinished: 12,
+        topic: 'HTML'
     },
     {
         startedAt:  date1 = new Date (2021, 1, 17, 13) ,
         finishedAt: date2 = new Date (2021, 1, 17, 20),
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '25',
-        tasksFinished: '22',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
+        tasksGiven: 25,
+        tasksFinished: 22,
         topic: 'JavaScript'
     },
     {
         startedAt: date1 = new Date (2021, 1, 18, 13) ,
         finishedAt: date2 = new Date (2021, 1, 18, 16) ,
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '10',
-        tasksFinished: '8',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
+        tasksGiven: 10,
+        tasksFinished: 8,
         topic: 'JavaScript'
     },
     {
         startedAt: date1 = new Date (2021, 1, 19, 12) ,
-        finishedAt: date2 =  = new Date (2021, 1, 19, 16),
-        totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
-        tasksGiven: '24',
-        tasksFinished: '18',
-        tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
-        topic: 'JavaScript'
+        finishedAt: date2 = new Date (2021, 1, 19, 16),
+        tasksGiven: 24,
+        tasksFinished: 18,
+        topic: 'HTML'
     }
        
 ]
 function totalTime(startedAt, finishedAt) {
-    return startedAt - finishedAt;
+    return startedAt.getHours() - finishedAt.getHours();
 }
 function tasksFinishedPercent(tasksGiven, tasksFinished) {
     return (tasksGiven / tasksFinished) * 100
 }
-for (let task in tasks)
+for (let task of tasks) {
+    task['totalTime'] = Math.abs(task.startedAt - task.finishedAt) / 3600000;
+    task['tasksFinishedPercent'] = Math.floor(task.tasksFinished / task.tasksGiven) * 100;
+    
+ }
+
+// totalTime: ${totalTime(this.startedAt, this.finishedAt)} ,
+// tasksFinishedPercent: ${tasksFinishedPercent(this.tasksGiven, this.tasksFinished)} ,
+console.log(tasks[1])
